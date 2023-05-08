c9ce7f0899b131b54612eee83be40fd52a539f89
Benny(Gibaek Han) <hkb31415@gmail.com>
2023-05-08 15:38:03 +0900

    [Add] BennyView1
    테스트용으로 BennyView1을 만들었습니다. 또 App에서 ContentView를 RootView로 변경하였습니다.

---
+++ GitPractice/GitPractice/Module/Benny/BennyView1.swift
@@ -0,0 +1,27 @@
+//
+//  BennyView1.swift
+//  GitPractice
+//
+//  Created by HAN GIBAEK on 2023/05/08.
+//
+
+import SwiftUI
+
+struct BennyView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct BennyView_Previews: PreviewProvider {
+    static var previews: some View {
+        BennyView()
+    }
+}
+
