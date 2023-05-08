c66acc2752780c883dc37eabdb18580ded3804be
sei <vanism7557@gmail.com>
2023-05-08 15:27:20 +0900

    feat: Initial project
    초기 프로젝트를 만들었어요.

---
+++ GitPractice/GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by sei on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
